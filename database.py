c0329114c8bc58693d9f469d9c167fc15b94c702
khalannil <141058165+khalannil@users.noreply.github.com>
2025-08-01 23:17:12 +0100

    Add database.py

---
+++ database.py
@@ -0,0 +1,35 @@
+import sqlite3
+
+DATABASE = './data/mimora.db'
+
+def connect_db():
+    return sqlite3.connect(DATABASE)
+
+def init_db():
+    with connect_db() as db:
+        cursor = db.cursor()
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS chronicles (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                title TEXT NOT NULL,
+                author TEXT NOT NULL,
+                status TEXT NOT NULL
+            );
+        ''')
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS archives (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL UNIQUE,
+                url TEXT NOT NULL
+            );
+        ''')
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS conduits (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL UNIQUE,
+                archive_id INTEGER,
+                FOREIGN KEY (archive_id) REFERENCES archives(id)
+            );
+        ''')
+        db.commit()
+
